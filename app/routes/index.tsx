25a6b45954eabe9e2985c8b565f9c20072a5fd58
Martin Beierling-Mutz <mbeierling@gmail.com>
2021-11-27 00:53:11 +0100

    init

---
+++ app/routes/index.tsx
@@ -0,0 +1,34 @@
+import type { MetaFunction } from "remix";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "D4R Dashboard",
+    description:
+      "Here you can enter your Ethereum Address and check your D4R NFT collection.",
+  };
+};
+
+export default function Index() {
+  return (
+    <div className="remix__page">
+      <main>
+        <h2>Welcome to D4R Dashboard!</h2>
+        <p>
+          Here you can enter your Ethereum Address and check your D4R NFT
+          collection.
+        </p>
+        <p>
+          Currently it's interesting to know whether you're eligible for the
+          upcoming airdrop of Developer DAO's ERC20 govenance token. This app
+          will let you know.
+        </p>
+      </main>
+      <aside>
+        <h2>Enter Address</h2>
+        <input type="text" />
+
+        <button>or Connect Wallet</button>
+      </aside>
+    </div>
+  );
+}
