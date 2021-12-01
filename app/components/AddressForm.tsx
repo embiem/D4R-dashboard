4c3ef23fb235d636daf28912468438e83902ebde
Martin Beierling-Mutz <mbeierling@gmail.com>
2021-12-01 01:30:33 +0100

    Get user's address

---
+++ app/components/AddressForm.tsx
@@ -0,0 +1,110 @@
+import { Form, useSubmit } from "@remix-run/react";
+import { useRef, useState } from "react";
+import { UsersIcon, SearchIcon } from "@heroicons/react/outline";
+import queryString from "query-string";
+
+declare let window: any;
+
+export default function AddressForm() {
+  const [enterManually, setEnterManually] = useState(false);
+
+  let submit = useSubmit();
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      submit(
+        { address: accounts[0] },
+        {
+          action: "/tools?index",
+          method: "post",
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className={`${!enterManually && "hidden"}`}>
+        <Form method="post">
+          <div>
+            <label
+              htmlFor="address"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Manually enter address
+            </label>
+            <div className="mt-1 inline-flex rounded-md shadow-sm">
+              <div className="relative flex items-stretch flex-grow focus-within:z-10">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <UsersIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+                <input
+                  type="text"
+                  name="address"
+                  id="address"
+                  className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300"
+                  placeholder=".ens or 0x"
+                />
+              </div>
+              <button
+                onClick={(e) => {
+                  //onAddressChange(accounts[0])
+                  console.log(e.currentTarget);
+                }}
+                type="submit"
+                className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+              >
+                <SearchIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <span>Submit</span>
+              </button>
+            </div>
+          </div>
+        </Form>
+
+        <button
+          onClick={() => {
+            setEnterManually(false);
+            connectWalletAction();
+          }}
+          className="ml-4 underline text-lg text-gray-600"
+        >
+          Or connect wallet
+        </button>
+      </div>
+
+      <div className={`${enterManually && "hidden"}`}>
+        <button
+          className="p-4 rounded-md text-indigo-900 font-bold bg-gradient-to-tr from-indigo-400 via-blue-400 to-sky-400"
+          onClick={connectWalletAction}
+        >
+          Connect Wallet To Get Started
+        </button>
+        <button
+          onClick={() => setEnterManually(true)}
+          className="ml-4 underline text-lg text-gray-600"
+        >
+          Or enter address manually
+        </button>
+      </div>
+    </>
+  );
+}
--- app/routes/tools/index.tsx
+++ app/routes/tools/index.tsx
@@ -1,8 +1,12 @@
 import type { MetaFunction } from "remix";
-import { Outlet } from "remix";
-import { InformationCircleIcon } from "@heroicons/react/outline";
+import { ActionFunction, json, useActionData } from "remix";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ethers } from "ethers";
 
 import GridList from "~/components/GridList";
+import AddressForm from "~/components/AddressForm";
+import { useEffect, useState } from "react";
+import invariant from "ts-invariant";
 
 export let meta: MetaFunction = () => {
   return {
@@ -12,9 +16,116 @@ export let meta: MetaFunction = () => {
   };
 };
 
+const APIURL = "https://api.thegraph.com/subgraphs/name/ensdomains/ens";
+
+const ensToAddressQuery = `
+  query($name: String) {
+    domains(where: {name: $name}) {
+      id
+      name
+      owner {
+        id
+      }
+      resolvedAddress {
+        id
+      }
+    }
+  }
+`;
+
+const addressToEnsQuery = `
+  query($address: String) {
+    domains(where: {resolvedAddress: $address}){
+      name
+    }
+  }
+  
+`;
+
+const client = new ApolloClient({
+  uri: APIURL,
+  cache: new InMemoryCache(),
+});
+
+export let action: ActionFunction = async ({ request, params }) => {
+  //if (request.method !== "PUT") throw json("Bad Request", { status: 400 });
+
+  let formData = await request.formData();
+  console.log(await request.text());
+  console.log(formData.entries());
+  console.log(formData.values());
+  const address = formData.get("address");
+
+  if (!address) throw json("Missing address", { status: 400 });
+
+  console.log(address);
+  invariant(typeof address === "string");
+
+  if (address.toLowerCase().endsWith(".eth")) {
+    // try to resolve it
+    try {
+      const { data }: any = await client.query({
+        query: gql(ensToAddressQuery),
+        variables: {
+          name: address,
+        },
+      });
+
+      console.log(data);
+
+      if (data.domains.length > 0) {
+        return {
+          address: data.domains[0].resolvedAddress.id,
+          domain: data.domains[0].name,
+        };
+      } else {
+        throw Error("Could not resolve .eth address.");
+      }
+    } catch (err: any) {
+      throw json(err.toString(), { status: 400 });
+    }
+  }
+
+  // validate that it's an existing Ethereum address
+  if (ethers.utils.isAddress(address)) {
+    try {
+      // check if ENS domain exists
+      const { data }: any = await client.query({
+        query: gql(addressToEnsQuery),
+        variables: {
+          address: address.toLowerCase(),
+        },
+      });
+
+      if (data.domains.length > 0) {
+        return {
+          address: address,
+          domain: data.domains[0].name,
+        };
+      } else {
+        throw Error("Could not resolve .eth address.");
+      }
+    } catch (err) {}
+
+    // Otherwise return just address
+    return {
+      address: address,
+      domain: null,
+    };
+  }
+
+  throw json("Bad Request", { status: 400 });
+};
+
 export default function Index() {
+  const { address, domain } = useActionData() || {};
+
   return (
     <div>
+      <div className="mb-8">
+        {!address ? <AddressForm /> : <div>👋 {domain || address}</div>}
+      </div>
+
       <GridList />
     </div>
   );
